1169b4741af0c68d6b9dda112654383571a969b8
shadownight2029 <42266783+shadownight2029@users.noreply.github.com>
2018-12-24 15:56:27 +0700

    class for single dashboard

---
+++ ClassDashboard.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Class1
+{
+    public class AssetAllocation
+    {
+        public string label { get; set; }
+        public string total { get; set; }
+        public string percent { get; set; }
+    }
+
+    public class NetWorth
+    {
+        public string InceptionDate { get; set; }
+        public string Yesterday { get; set; }
+        public string InceptionDate { get; set; }
+        public string ThisMonth { get; set; }
+        public string ThisYear { get; set; }
+        public string ThisMonth { get; set; }
+        public string AllTime { get; set; }
+        public string ExpenseRatio { get; set; }
+        public string TotalDividends { get; set; }
+        public string EstimatedDividendYield { get; set; }
+    }
+
+    public class AnnualizedStandardDeviation
+    {
+        public string Label { get; set; }
+        public string Standard { get; set; }
+        public string OneMonth { get; set; }
+        public string SixMonth { get; set; }
+        public string YTD { get; set; }
+        public string OneYear { get; set; }
+        public string ThreeYars { get; set; }
+        public string ThreeSinceinception { get; set; }
+    }
+}
